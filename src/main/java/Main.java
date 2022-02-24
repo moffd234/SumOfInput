838346009fefac2e81317e55bfda799afe25e9e7
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-02-24 11:32:04 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,4 +6,16 @@ public class Main {
     public static void main(String[] args){
 
     }
+    
+    int sumOfNumbers(int n) {
+        return 0;
+    }
+    
+    // for Extra Credit
+    int gaussianSumOfNumbers(int n) {
+        return 0;
+    }
+   
+    // for Extra extra credit - compare the methods and show which one is faster
+    
 }
