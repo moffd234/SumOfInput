95d39f9ded63d68b0d11a8a943a9ab4721ab0b82
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-11-08 15:31:03 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,33 @@
 public class Main {
 
     public static void main(String[] args){
+        
+        int number = getInputNumber();
+        
+        int sum = sumOfNumbers(number);
+        
+        System.out.println(sum);
 
     }
     
     // do this one and the lab is complete. really.
-    
+    // return the sum of 0 to n... if n == 3, the result should be 6
     int sumOfNumbers(int n) {
         return 0;
     }
     
     
-    
+    int getInputNumber() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+
+        // This method reads the number provided using keyboard
+        int num = scan.nextInt();
+
+        // Closing Scanner after the use
+        scan.close();
+        return num;
+    }
     
     
     
